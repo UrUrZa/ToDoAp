097b28bbf2722e850d31fafd749cfc54fb6ca02d
UrUrZa <IshvarIndrovich@mail.ru>
2022-05-20 18:55:00 +0400

    fisrt controllers

--- src/main/java/Controllers/ToDoListController.java
+++ src/main/java/Controllers/ToDoListController.java
@@ -7,6 +7,7 @@ import UseCases.CaseGet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,32 +16,22 @@ import java.time.LocalDate;
 
 
 @SpringBootApplication
+@ComponentScan( basePackages = {"UseCases"})
 @RestController
 @RequestMapping(path = "api/v1/ToDoList")
 
 
-public class ToDoListController{
+public class ToDoListController {
 
         public static void main(String[] args) {
                 SpringApplication.run(ToDoListController.class, args);
         }
 
-@GetMapping
-public String getToDoList (){
-        ToDoList test = new ToDoList("Do it now");
-        System.out.println(test.getName());
-//    test.printList();
-        test.AddToDo(new ToDo("отдохнуть", LocalDate.now()));
-        test.AddToDo(new ToDo("run", Type.sport,LocalDate.EPOCH));
-      return   test.toString();
+        @Autowired
+        @GetMapping
+        public ToDoList getToDoList(CaseGet caseGet) {
+                return caseGet.getToDoList();
+
+        }
 }
-}
-
-
-
-/*
-public ToDoList getToDoList (){
-        return caseGet.getToDoList();
-
-}*/
 
