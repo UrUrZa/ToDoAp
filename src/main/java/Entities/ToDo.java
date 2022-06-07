9ecde0d27d9669e528cd08712ab9e46132db50be
UrUrZa <IshvarIndrovich@mail.ru>
2022-06-07 17:57:27 +0400

    database configuration added

--- src/main/java/Entities/ToDo.java
+++ src/main/java/Entities/ToDo.java
@@ -1,13 +1,33 @@
 package Entities;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 
-
+@Entity
+@Table(name = "todo")
 public class ToDo {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Column(name = "name", nullable = false, length = 128)
     private String name;
+
+    @Column(name = "isComplited", nullable = true)
     private boolean isComplited = false;
+    @Column(name = "date", nullable = true, length = 128)
     private LocalDate date;
+    @Column(name = "type", nullable = true, length = 128)
     private Type type = Type.other;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
 
     public String getName() {
@@ -42,6 +62,16 @@ public class ToDo {
         this.type = type;
     }
 
+    public ToDo() {
+    }
+    public ToDo(Integer id, String name, boolean isComplited, LocalDate date, Type type) {
+        this.id = id;
+        this.name = name;
+        this.isComplited = isComplited;
+        this.date = date;
+        this.type = type;
+    }
+
     public ToDo(String name, Type type, boolean isComplited, LocalDate date) {
         this.name = name;
         this.isComplited = isComplited;
@@ -62,7 +92,7 @@ public class ToDo {
 
     @Override
     public String toString() {
-        return "ToDo{" + name + '\'' +
+        return "ToDo{"+String.valueOf(id)+" " + name + '\'' +
                 ", type=" + type +
                 ", date=" + date +
                 ", isComplited=" + isComplited +
